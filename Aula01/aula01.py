48ae06f4b76003d8bb630d3d7e8cf58c5505dbbb
Alexandre de Souza Eustaquio <alexeus.souza@gmail.com>
2022-10-05 15:50:29 -0300

    Update aula01.py

--- Aula01/aula01.py
+++ Aula01/aula01.py
@@ -8,10 +8,14 @@ import re
 # sub => substitui um valor dentro do texto
 # compile => compila expressoes regulares
 
-string = 'Este é um teste de expressões regulares.'
+string = 'Este é um teste de expressões regulares, este teste pode ser util.'
 
 print(re.search(r'teste', string)) # usar re.funcao(r'valor procurado', variavel)
 # retorna uma expressão Match informando onde a palavra inicia e onde termina dentro da string
 
+print(re.findall(r'teste', string)) # usar re.funcao(r'valor procurado', variavel)
+# pesquisa a palavra teste na string e retorna uma lista com o resultado da expressão 
 
+print(re.sub(r'teste', 'ABCD', string)) # usar re.funcao(r'valor procurado', variavel)
+# substitui a palavra teste por ABCD na string informada na expressão 
 
